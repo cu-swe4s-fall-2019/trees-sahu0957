d6efce6fb12306a7415ee61dee556688ee7a4b52
sahu0957 <sahu0957@colorado.edu>
2019-11-08 00:23:29 +0000

    added testing, new submodule

---
+++ test_binary_tree.py
@@ -0,0 +1,13 @@
+import unittest
+import binary_tree
+import random
+import os
+from os import path
+
+class TestBinaryTree(unittest.TestCase):
+
+    def test_binary_tree_first_entry(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()

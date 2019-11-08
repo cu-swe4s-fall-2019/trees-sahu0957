2435fd1347398fa8107163b4d3141415881298df
sahu0957 <sahu0957@colorado.edu>
2019-11-08 04:31:36 +0000

    added avl_tree submod, testing, insert function

--- binary_tree.py
+++ binary_tree.py
@@ -1,8 +1,26 @@
 class Node:
     def __init__(self, key, value=None, left=None, right=None):
+        self.key = key
+        self.value = value
+        self.left = left
+        self.right = right
 
 def insert(root, key, value=None):
-    return root
+    if root == None:
+        root = Node(key, value=value)
+        return root
+    else:
+        if key < root.key:
+            if root.left == None:
+                root.left = Node(key, value = value)
+            else:
+                insert(root.left, key, value = value)
+        else:
+            if root.right == None:
+                root.right = Node(key, value = value)
+            else:
+                insert(root.right, key, value = value)
+        return root
 
 def search(root, key):
     return None
--- test_binary_tree.py
+++ test_binary_tree.py
@@ -7,7 +7,29 @@ from os import path
 class TestBinaryTree(unittest.TestCase):
 
     def test_binary_tree_first_entry(self):
-        pass
+        r = binary_tree.insert(root = None, key="key", value=1)
+        self.assertEqual((r.key,r.value), ("key", 1))
+
+    def test_binary_tree_insert_left(self):
+        root = None
+        root = binary_tree.insert(root, key = 10)
+        root = binary_tree.insert(root, key = 9)
+        self.assertEqual(root.left.key, 9)
+
+    def test_binary_tree_insert_right(self):
+        root = None
+        root = binary_tree.insert(root, key = 10)
+        root = binary_tree.insert(root, key = 11)
+        self.assertEqual(root.right.key, 11)
+
+    def test_binary_tree_multiple_right(self):
+        root = None
+        root = binary_tree.insert(root, key = 10)
+        root = binary_tree.insert(root, key = 12)
+        root = binary_tree.insert(root, key = 11)
+        root = binary_tree.insert(root, key = 13)
+        self.assertEqual(root.right.right.key, 13)
+        self.assertEqual(root.right.left.key, 11)
 
 if __name__ == '__main__':
     unittest.main()
